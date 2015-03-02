366a620ced0c81ddf429d6009e5ecf8b45aa4355
your_username <your_email@example.com>
2015-03-02 16:03:08 -0500

    added function to handle missing dates

--- R/laycUtils.R
+++ R/laycUtils.R
@@ -30,3 +30,17 @@ load_txt <- function(file) {
   
   return(out)
 }
+
+#' assign_date()
+#'
+#' Helper function. Assign date to missing records
+#' @param dates character vector: vector of date characters
+#' @param new_date character vector: Represent the date to be assigned to missing values
+#' @return vector
+#' @keywords internal
+#' @export
+#' @examples
+#' assign_date(my_dates, new_date = '01/01/2015')
+assign_date <- function(dates, new_date) {
+  dates[is.na(dates)] <- new_date
+}

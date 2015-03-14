1d3bc673448518a19ec08f556b963ff2bf01ab1e
tonyfujs <tonyfujs@gmail.com>
2015-03-14 13:52:02 -0400

    added helper function to clean data set from [admin] eto reports

---
+++ R/clean_data.R
@@ -0,0 +1,31 @@
+#' clean_data()
+#'
+#' This function conducts basic data cleaning tasks on a data frame.
+#' @param data dataframe: a dataframe containing.
+#' @param to_date character vector: Names of columns to be converted to date format.
+#' @param to_numeric character vector: Names of columns to be converted to numeric format.
+#' @return dataframe
+#' @export
+#' @examples
+#' enroll <- laycUtils::load_txt('./my_data_folder/enrollment.txt')
+#' clean_data(data = enroll)
+
+clean_data <- function(data,
+                     to_date = c('start', 'end', 'date'),
+                     to_numeric = c('days_enrolled', 'time')) {
+  # Turn everything to lower case to make data manipulation easier
+  data[] <- lapply(data, tolower)
+  # Turn date columns to date format
+  to_date <- intersect(to_date, colnames(data))
+  data[to_date] <- lapply(data[to_date], lubridate::mdy)
+  # Turn numeric columns to numeric
+  to_numeric <- intersect(to_numeric, colnames(data))
+  num_columns <- stringr::str_detect(colnames(data), 'id$')
+  num_columns <- colnames(data)[num_columns]
+  num_columns <- c(num_columns, to_numeric)
+  data[num_columns] <- lapply(data[num_columns], tidyr::extract_numeric)
+  
+  return(data)
+}  
+  
+ 

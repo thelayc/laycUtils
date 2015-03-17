a5e3733bb4556f362ae9f6308962f4084c20fff5
your_username <your_email@example.com>
2015-03-17 11:25:39 -0400

    Added ... argument to allow more flexibility to load functions

--- R/laycUtils.R
+++ R/laycUtils.R
@@ -2,13 +2,14 @@
 #'
 #' Helper function. Set default options for the read.csv function
 #' @param file character vector: the name of the file which the data are to be read from. Each row of the table appears as one line of the file. If it does not contain an absolute path, the file name is relative to the current working directory, getwd().
+#' @param ... Other arguments to be passed to load_csv(). See ?read.table to get the list of possible arguments
 #' @return data frame
 #' @keywords internal
 #' @export
 #' @examples
 #' load_csv('./data/my_csv_file.csv')
 
-load_csv <- function(file) {
+load_csv <- function(file, ...) {
   out <- read.csv(file, header = TRUE, stringsAsFactors = FALSE, na.strings = "", colClasses = "character")
   
   return(out)
@@ -18,12 +19,13 @@ load_csv <- function(file) {
 #'
 #' Helper function. Set default options for the read.table function
 #' @param file character vector: path .txt file containing data to be loaded. Each row of the table appears as one line of the file. If it does not contain an absolute path, the file name is relative to the current working directory, getwd().
+#' @param ... Other arguments to be passed to load_txt(). See ?read.table to get the list of possible arguments
 #' @return data frame
 #' @keywords internal
 #' @export
 #' @examples
 #' load_txt('./data/my_txt_file.txt')
-load_txt <- function(file) {
+load_txt <- function(file, ...) {
   out <- read.table(file, header = TRUE, sep = '\t', quote = "", comment.char = "", na.strings = "", stringsAsFactors = FALSE, colClasses = "character")
   
   return(out)

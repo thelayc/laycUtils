c616a6ea85ee5718e72bb76fdf66c3d3ba66c6a9
your_username <your_email@example.com>
2015-03-17 11:30:49 -0400

    fixed bug with ...

--- R/laycUtils.R
+++ R/laycUtils.R
@@ -10,7 +10,7 @@
 #' load_csv('./data/my_csv_file.csv')
 
 load_csv <- function(file, ...) {
-  out <- read.csv(file, header = TRUE, stringsAsFactors = FALSE, na.strings = "", colClasses = "character")
+  out <- read.csv(file, header = TRUE, stringsAsFactors = FALSE, na.strings = "", colClasses = "character", ...)
   
   return(out)
 }
@@ -26,7 +26,7 @@ load_csv <- function(file, ...) {
 #' @examples
 #' load_txt('./data/my_txt_file.txt')
 load_txt <- function(file, ...) {
-  out <- read.table(file, header = TRUE, sep = '\t', quote = "", comment.char = "", na.strings = "", stringsAsFactors = FALSE, colClasses = "character")
+  out <- read.table(file, header = TRUE, sep = '\t', quote = "", comment.char = "", na.strings = "", stringsAsFactors = FALSE, colClasses = "character", ...)
   
   return(out)
 }

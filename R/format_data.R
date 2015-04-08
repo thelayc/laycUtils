e628c2d522f5435539ffd87633cebb7604ed8d8f
your_username <your_email@example.com>
2015-04-08 14:32:26 -0400

    Made messages from format_data() more readable

--- R/format_data.R
+++ R/format_data.R
@@ -14,16 +14,16 @@ format_data <- function(data,
                      to_date = c('start$|^start|end$|^end|date'),
                      to_numeric = c('id$|days|time|weight|wage')) {
   # Turn everything to lower case to make data manipulation easier
-  message('Passing all variables to lower case...')
+  message('Passing all variables to lower case...\n')
   data[] <- lapply(data, tolower)
   # Turn date columns to date format
   to_date <- colnames(data)[grepl(to_date, colnames(data))]
-  message(paste('Formatting these variables as date:\n', 
+  message(paste('\nFormatting these variables as date:\n', 
                 paste(to_date, collapse = "\n")))
   data[to_date] <- lapply(data[to_date], lubridate::mdy)
   # Turn numeric columns to numeric
   to_numeric <- colnames(data)[grepl(to_numeric, colnames(data))]
-  message(paste('Formatting these variables as numeric:\n', 
+  message(paste('\nFormatting these variables as numeric:\n', 
                 paste(to_numeric, collapse = "\n")))
   data[to_numeric] <- lapply(data[to_numeric], tidyr::extract_numeric)
   

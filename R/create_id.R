90cd6d01a8df6444bfaea0ea9dd55cf15133926c
your_username <your_email@example.com>
2015-06-19 14:39:07 -0400

    add create_id() function

---
+++ R/create_id.R
@@ -0,0 +1,23 @@
+#' create_id()
+#'
+#' Helper function. Create custom id by concatenating different variables
+#' @param var character vector: list of variables to be concatenated to form the new id
+#' @param df dataframe
+#' @return character vector
+#' @keywords internal
+#' @export
+#' @examples
+#' create_id(df, c('first_name', 'last_name')
+
+create_id <- function(df, var = c("first_name", "last_name")) {
+  # check that var contains valid variable names from df var %in% colnames(df)
+  stopifnot(all(var %in% colnames(df)))
+  # concatenate create a new column `x` with the three columns collapsed together
+  out <- do.call(paste0, df[var])
+  # Remove all non alphabetic characters (including spaces)
+  out <- gsub("[^[:alpha:]]", "", out)
+  # Change to lowercase id_name
+  out <- tolower(out)
+  
+  return(out)
+}
